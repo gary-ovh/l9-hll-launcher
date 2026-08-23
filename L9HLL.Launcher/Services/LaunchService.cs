114d8d919ab668d73f3dd4354651cdcf1a9c2eec
gary-ovh <gary@ovh.net>
2026-08-23 01:17:20 -0400

    Increase key press delays - wait 15s initial + 800ms between keys

--- L9HLL.Launcher/Services/LaunchService.cs
+++ L9HLL.Launcher/Services/LaunchService.cs
@@ -11,9 +11,63 @@ namespace L9HLL.Launcher.Services
     public class LaunchService
     {
         private const int HLL_AppId = 686810;
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct INPUT
+        {
+            public int Type;
+            public InputUnion U;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct InputUnion
+        {
+            [FieldOffset(0)]
+            public MOUSEINPUT mi;
+            [FieldOffset(0)]
+            public KEYBDINPUT ki;
+            [FieldOffset(0)]
+            public HARDWAREINPUT hi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct KEYBDINPUT
+        {
+            public short wVk;
+            public short wScan;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct HARDWAREINPUT
+        {
+            public int uMsg;
+            public short wParamL;
+            public short wParamH;
+        }
+
+        private const int INPUT_KEYBOARD = 1;
+        private const int KEYEVENTF_KEYUP = 0x0002;
         private const int VK_SPACE = 0x20;
         private const int VK_RETURN = 0x0D;
-        private const uint KEYEVENTF_KEYUP = 0x0002;
+        private const int VK_ESCAPE = 0x1B;
+        private const int VK_F1 = 0x70;
+
+        [DllImport("user32.dll")]
+        private static extern uint SendInput(uint nInputs, ref INPUT pInputs, int cbSize);
 
         [DllImport("user32.dll")]
         private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
@@ -22,7 +76,10 @@ namespace L9HLL.Launcher.Services
         private static extern bool IsWindow(IntPtr hWnd);
 
         [DllImport("user32.dll")]
-        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, IntPtr dwExtraInfo);
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
         [DllImport("user32.dll")]
         private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
@@ -34,13 +91,13 @@ namespace L9HLL.Launcher.Services
         private static extern bool AttachThreadInput(uint idAttach, uint idAttachTo, bool fAttach);
 
         [DllImport("user32.dll")]
-        private static extern IntPtr GetFocus();
+        private static extern IntPtr GetForegroundWindow();
 
         [DllImport("user32.dll")]
-        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        private static extern bool SetActiveWindow(IntPtr hWnd);
 
         [DllImport("user32.dll")]
-        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, IntPtr dwExtraInfo);
 
         public async void LaunchServer(ServerStatus server)
         {
@@ -74,30 +131,78 @@ namespace L9HLL.Launcher.Services
 
         private async Task AutoPressConnect()
         {
-            for (int i = 0; i < 60; i++)
+            // Wait at least 15 seconds for game to launch through Steam
+            for (int i = 0; i < 15; i++)
             {
                 await Task.Delay(1000);
+            }
 
+            // Now poll for the game window and send keys
+            for (int i = 0; i < 90; i++)
+            {
                 var gameWindow = FindGameWindow();
                 if (gameWindow != IntPtr.Zero && IsWindow(gameWindow))
                 {
                     SetForegroundWindow(gameWindow);
-                    await Task.Delay(300);
+                    await Task.Delay(1000);
 
-                    SendKey(VK_RETURN);
-                    await Task.Delay(200);
-                    SendKey(VK_RETURN);
+                    // Send multiple keys with longer pauses
+                    for (int j = 0; j < 5; j++)
+                    {
+                        SendKeyboardInput(VK_SPACE);
+                        await Task.Delay(800);
+                    }
+                    for (int j = 0; j < 5; j++)
+                    {
+                        SendKeyboardInput(VK_RETURN);
+                        await Task.Delay(800);
+                    }
+                    for (int j = 0; j < 5; j++)
+                    {
+                        SendKeyboardInput(VK_ESCAPE);
+                        await Task.Delay(800);
+                    }
                     return;
                 }
+                await Task.Delay(1000);
             }
+        }
+
+        private void SendKeyboardInput(int vkCode)
+        {
+            INPUT inputDown = new INPUT();
+            inputDown.Type = INPUT_KEYBOARD;
+            inputDown.U.ki.wVk = (short)vkCode;
+            inputDown.U.ki.dwFlags = 0;
+
+            INPUT inputUp = new INPUT();
+            inputUp.Type = INPUT_KEYBOARD;
+            inputUp.U.ki.wVk = (short)vkCode;
+            inputUp.U.ki.dwFlags = KEYEVENTF_KEYUP;
+
+            SendInput(1, ref inputDown, Marshal.SizeOf(typeof(INPUT)));
+            SendInput(1, ref inputUp, Marshal.SizeOf(typeof(INPUT)));
         }
 
         private IntPtr FindGameWindow()
         {
             try
             {
-                var processes = Process.GetProcessesByName("Hell Let Loose");
+                var processes = Process.GetProcessesByName("HLL");
                 foreach (var proc in processes)
+                {
+                    if (proc.MainWindowHandle != IntPtr.Zero && IsWindow(proc.MainWindowHandle))
+                    {
+                        return proc.MainWindowHandle;
+                    }
+                }
+            }
+            catch { }
+
+            try
+            {
+                var launchProcesses = Process.GetProcessesByName("Launch_HLL");
+                foreach (var proc in launchProcesses)
                 {
                     if (proc.MainWindowHandle != IntPtr.Zero && IsWindow(proc.MainWindowHandle))
                     {
@@ -112,29 +217,6 @@ namespace L9HLL.Launcher.Services
                 return hllWindow;
 
             return IntPtr.Zero;
-        }
-
-        private void SendKey(int vkCode)
-        {
-            try
-            {
-                GetWindowThreadProcessId(GetFocus(), out uint threadId);
-                uint currentThreadId = GetCurrentThreadId();
-
-                if (threadId != currentThreadId)
-                {
-                    AttachThreadInput(threadId, currentThreadId, true);
-                }
-
-                keybd_event((byte)vkCode, 0, 0, IntPtr.Zero);
-                keybd_event((byte)vkCode, 0, KEYEVENTF_KEYUP, IntPtr.Zero);
-
-                if (threadId != currentThreadId)
-                {
-                    AttachThreadInput(threadId, currentThreadId, false);
-                }
-            }
-            catch { }
         }
 
         private static string? FindGamePath()
